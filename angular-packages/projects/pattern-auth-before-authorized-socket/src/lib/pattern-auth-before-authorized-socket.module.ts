3d09d9ac43c2b6d1ab0db37c4010b8f9c9e66a9e
santalov <santalov.stas@gmail.com>
2022-03-21 11:38:28 +0300

    provider for pattern module

--- angular-packages/projects/pattern-auth-before-authorized-socket/src/lib/pattern-auth-before-authorized-socket.module.ts
+++ angular-packages/projects/pattern-auth-before-authorized-socket/src/lib/pattern-auth-before-authorized-socket.module.ts
@@ -1,4 +1,6 @@
-import {NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {WebSocketChain} from './models/web-socket-chain';
+import {WEB_SOCKET_CHAIN} from './constants/di-token';
 
 
 @NgModule({
@@ -7,5 +9,17 @@ import {NgModule} from '@angular/core';
   exports: []
 })
 export class PatternAuthBeforeAuthorizedSocketModule {
-  static forRoot(): void {}
+  static forRoot<RequestType, ResponseType, UnderlyingDataType extends string | ArrayBufferLike | Blob | ArrayBufferView, UserInfo>(
+    chain: WebSocketChain<RequestType, ResponseType, UnderlyingDataType, UserInfo>
+  ): ModuleWithProviders<PatternAuthBeforeAuthorizedSocketModule> {
+    return {
+      ngModule: PatternAuthBeforeAuthorizedSocketModule,
+      providers: [
+        {
+          provide: WEB_SOCKET_CHAIN,
+          useValue: chain,
+        }
+      ]
+    }
+  }
 }
