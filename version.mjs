8c3f2175e8a37e8a9911874c960aa1f9fc221ad6
santalov <santalov.stas@gmail.com>
2022-06-13 13:56:33 +0300

    versioning script

---
+++ version.mjs
@@ -0,0 +1,42 @@
+import * as fs from 'node:fs/promises'
+
+const foldersWithProjects = [
+    'packages',
+    'angular-packages/projects'
+]
+
+const version = process.argv[2]
+
+if (version && version.match(/\d+\.\d+\.\d+/)) {
+    for (const folder of foldersWithProjects) {
+        const dir = await fs.opendir(folder)
+        for await (const dirent of dir) {
+            if (dirent.isDirectory()) {
+                const packageJsonPath = folder + '/' + dirent.name + '/package.json'
+                let fileHandle;
+                let config;
+                try {
+                    fileHandle = await fs.open(packageJsonPath, 'r')
+                    const content = await fileHandle.readFile({encoding: 'utf8'})
+                    config = JSON.parse(content)
+                } finally {
+                    await fileHandle?.close()
+                }
+                if (config) {
+                    config.version = version
+                    let fileHandleWrite
+                    try {
+                        fileHandleWrite = await fs.open(packageJsonPath, 'w')
+                        await fileHandleWrite.writeFile(JSON.stringify(config, null, 2), {encoding: 'utf8'})
+                        console.log(dirent.name, version)
+                    } finally {
+                        fileHandleWrite?.close()
+                    }
+                }
+            }
+        }
+    }
+
+} else {
+    console.error('provide version as argument in form of X.X.X')
+}
