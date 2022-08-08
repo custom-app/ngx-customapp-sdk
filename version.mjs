fbf6ed01b0d48a4ea5d25f3519e26efeed11b593
santalov <santalov.stas@gmail.com>
2022-08-08 20:54:47 +0300

    version.mjs script knows about root package.json. New version

--- version.mjs
+++ version.mjs
@@ -9,10 +9,41 @@ import * as fs from 'node:fs/promises'
 
 const foldersWithProjects = [
     'packages',
-    'angular-packages/projects'
+    'angular-packages/projects',
+]
+
+const extraPackages = [
+    './package.json' // root package.json must be in sync to represent overall project version
 ]
 
 const version = process.argv[2]
+
+async function handlePackageJson(packageJsonPath, dirent) {
+    let fileHandle;
+    let config;
+    try {
+        fileHandle = await fs.open(packageJsonPath, 'r')
+        const content = await fileHandle.readFile({encoding: 'utf8'})
+        config = JSON.parse(content)
+    } finally {
+        await fileHandle?.close()
+    }
+    if (config) {
+        config.version = version
+        let fileHandleWrite
+        try {
+            fileHandleWrite = await fs.open(packageJsonPath, 'w')
+            await fileHandleWrite.writeFile(JSON.stringify(config, null, 2), {encoding: 'utf8'})
+            if (dirent) {
+                console.log(dirent.name, version)
+            } else {
+                console.log(packageJsonPath, version)
+            }
+        } finally {
+            fileHandleWrite?.close()
+        }
+    }
+}
 
 if (version && version.match(/\d+\.\d+\.\d+/)) {
     for (const folder of foldersWithProjects) {
@@ -20,28 +51,12 @@ if (version && version.match(/\d+\.\d+\.\d+/)) {
         for await (const dirent of dir) {
             if (dirent.isDirectory()) {
                 const packageJsonPath = folder + '/' + dirent.name + '/package.json'
-                let fileHandle;
-                let config;
-                try {
-                    fileHandle = await fs.open(packageJsonPath, 'r')
-                    const content = await fileHandle.readFile({encoding: 'utf8'})
-                    config = JSON.parse(content)
-                } finally {
-                    await fileHandle?.close()
-                }
-                if (config) {
-                    config.version = version
-                    let fileHandleWrite
-                    try {
-                        fileHandleWrite = await fs.open(packageJsonPath, 'w')
-                        await fileHandleWrite.writeFile(JSON.stringify(config, null, 2), {encoding: 'utf8'})
-                        console.log(dirent.name, version)
-                    } finally {
-                        fileHandleWrite?.close()
-                    }
-                }
+                await handlePackageJson(packageJsonPath, dirent)
             }
         }
+    }
+    for (const packageJsonPath of extraPackages) {
+        await handlePackageJson(packageJsonPath)
     }
 
 } else {
