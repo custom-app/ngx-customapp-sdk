9062403030e0483d372338ef5855a3feef0ef187
santalov <santalov.stas@gmail.com>
2022-08-08 19:31:33 +0300

    docs

--- angular-packages/projects/ngx-customapp-errors/src/lib/models/errors-reporter.ts
+++ angular-packages/projects/ngx-customapp-errors/src/lib/models/errors-reporter.ts
@@ -3,7 +3,7 @@ import {ContextError} from './context-error';
 
 /**
  * Should send the report to backend. Must not require authorization. Be aware to disable
- * auth interceptor, if you are using JwtInterceptor, provided by the ngx-customapp-jwt package
+ * auth interceptor, if you are using JwtInterceptor, provided by the ngx-customapp-jwt package.
  */
 export abstract class ErrorsReporter {
   abstract report(error: ContextError): Observable<void>
